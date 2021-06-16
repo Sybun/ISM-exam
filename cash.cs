601109353524a0a607518d7bdf3082f0d654881b
Sybun <72869210+Sybun@users.noreply.github.com>
2021-06-16 19:06:07 +0300

    Update cash.cs
    Pamokos darbas :)

--- cash.cs
+++ cash.cs
@@ -1,6 +1,36 @@
 using System;
 class HelloWorld {
   static void Main() {
-    Console.WriteLine("Hello World");
+    string userInputString = Console.ReadLine();
+    float userInputFloat = float.Parse(userInputString);
+    int userInputInt = (int)Math.Round(userInputFloat * 100);
+    int counter = 0;
+    
+while(userInputInt > 0)
+{
+    if(userInputInt >= 25)
+    {
+        counter = counter + 1;
+        userInputInt= userInputInt - 25;
+    }
+    else if(userInputInt >= 10)
+    {
+        counter = counter + 1;
+        userInputInt= userInputInt - 10;
+    }
+    else if(userInputInt >= 5)
+    {
+        counter = counter + 1;
+        userInputInt= userInputInt - 5 ;
+    }
+    else if(userInputInt >= 1)
+    {
+        counter = counter + 1;
+        userInputInt= userInputInt - 1;
+    }
+}
+    
+    
+    Console.WriteLine(counter);
   }
 }
